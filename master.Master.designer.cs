329cc04107306fd4044254f63dcebecbe08986fa
Shahin Katebi <me@shaahin.us>
2012-09-29 16:03:06 +0330

    Added user count and a URL Rewrite rule for /username urls to redirect to Twitter.

--- master.Master.designer.cs
+++ master.Master.designer.cs
@@ -47,5 +47,14 @@ namespace shTwiX {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// lit_activeUsers control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal lit_activeUsers;
     }
 }
